b0af16375e735ca8adabae69002a4eb549698dfb
YuFeng <leo20145kl@gmail.com>
2021-02-04 15:54:00 +0800

    Add a series of Update conversations
    To be done:
    - Change to: start first, then branch to share and update
    - Photos will be replaced
    - Haven't broadcast to channels

--- bot.py
+++ bot.py
@@ -33,7 +33,7 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-PANTRY, PERISHABLE, PHOTO, FEEDBACK = range(4)
+PANTRY_START, PERISHABLE_START, PHOTO_START, PANTRY_UPDATE, PHOTO_UPDATE, FEEDBACK_START, FEEDBACK_UPDATE = range(7)
 
 
 def start(update: Update, context: CallbackContext) -> int:
@@ -41,15 +41,15 @@ def start(update: Update, context: CallbackContext) -> int:
 
     update.message.reply_text(
         'Hi! My name is RVRC Famyshd.'
-        'Which pantry did you put your unopened food in?'
+        'Which pantry did you put your unopened food in? '
         'Send /cancel to stop the sharing.\n\n',
         reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
     )
 
-    return PANTRY
+    return PANTRY_START
 
 
-def pantry(update: Update, context: CallbackContext) -> int:
+def pantry_start(update: Update, context: CallbackContext) -> int:
     reply_keyboard = [['Perishable', 'Non-perishable']]
 
     user = update.message.from_user
@@ -60,15 +60,15 @@ def pantry(update: Update, context: CallbackContext) -> int:
         reply_markup=ReplyKeyboardRemove(),
     )
     update.message.reply_text(
-        'Is the food perishable or non-perishable?'
+        'Is the food perishable or non-perishable? '
         'Send /cancel to stop the sharing.\n\n',
         reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
     )
 
-    return PERISHABLE
+    return PERISHABLE_START
 
 
-def perishable(update: Update, context: CallbackContext) -> int:
+def perishable_start(update: Update, context: CallbackContext) -> int:
     user = update.message.from_user
     perishable = update.message.text
     logger.info("The food shared is %s.", perishable)
@@ -77,27 +77,27 @@ def perishable(update: Update, context: CallbackContext) -> int:
         reply_markup=ReplyKeyboardRemove(),
     )
     update.message.reply_text(
-        'Please send me a photo of the food you want to share.'
+        'Please send me a photo of the food you want to share. '
         'Send /cancel to stop the sharing.\n\n'
     )
 
-    return PHOTO
+    return PHOTO_START
 
 
-def photo(update: Update, context: CallbackContext) -> int:
+def photo_start(update: Update, context: CallbackContext) -> int:
     user = update.message.from_user
     photo_file = update.message.photo[-1].get_file()
     photo_file.download('user_photo.jpg')
     logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
     update.message.reply_text(
-        'Gorgeous! Lastly, do you have anything to tell us?'
+        'Gorgeous! Lastly, do you have anything to tell us? '
         'or send /skip if you don\'t have anything to say.',
     )
 
-    return FEEDBACK
+    return FEEDBACK_START
 
 
-def feedback(update: Update, context: CallbackContext) -> int:
+def feedback_start(update: Update, context: CallbackContext) -> int:
     user = update.message.from_user
     feedback = update.message.text
     logger.info(
@@ -110,7 +110,7 @@ def feedback(update: Update, context: CallbackContext) -> int:
     return ConversationHandler.END
 
 
-def skip_feedback(update: Update, context: CallbackContext) -> int:
+def skip_feedback_start(update: Update, context: CallbackContext) -> int:
     user = update.message.from_user
     logger.info("User %s did not send a feedback.", user.first_name)
     update.message.reply_text(
@@ -120,19 +120,69 @@ def skip_feedback(update: Update, context: CallbackContext) -> int:
     return ConversationHandler.END
 
 
-'''
-def help(update: Update, context: CallbackContext) -> int:
+def status(update: Update, context: CallbackContext) -> int:
+    reply_keyboard = [['Block D', 'Block E']]
+
     update.message.reply_text(
-        'This is a bot to encourage food sharing culture in RVRC.\n'
-        'You can share your unopened food here.\n\n'
-        '/start - Start sharing food.\n'
-        '/help - View instructions.'
+        'Hi! My name is RVRC Famyshd.'
+        'Which pantry did you take the food from? '
+        'Send /cancel to stop the updating.\n\n',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
     )
 
+    return PANTRY_UPDATE
 
-def unknown(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
-'''
+
+def pantry_update(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    pantry = update.message.text
+    logger.info("%s update the food sharing at %s pantry.", user.first_name, pantry)
+    update.message.reply_text(
+        'Noted.',
+        reply_markup=ReplyKeyboardRemove(),
+    )
+    update.message.reply_text(
+        'Please send me a photo of the food after you take. '
+        'Send /cancel to stop the sharing.\n\n'
+    )
+
+    return PHOTO_UPDATE
+
+
+def photo_update(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    photo_file = update.message.photo[-1].get_file()
+    photo_file.download('user_photo.jpg')
+    logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
+    update.message.reply_text(
+        'Gorgeous! Lastly, do you have anything to tell us? '
+        'or send /skip if you don\'t have anything to say.',
+    )
+
+    return FEEDBACK_UPDATE
+
+
+def feedback_update(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    feedback = update.message.text
+    logger.info(
+        "%s\'s feedback: %s", user.first_name, feedback
+    )
+    update.message.reply_text(
+        'Thank you very much for your feedback! Please also share food some day.'
+    )
+
+    return ConversationHandler.END
+
+
+def skip_feedback_update(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info("User %s did not send a feedback.", user.first_name)
+    update.message.reply_text(
+        'Thank you. Please also share food some day.'
+    )
+
+    return ConversationHandler.END
 
 
 def cancel(update: Update, context: CallbackContext) -> int:
@@ -153,23 +203,35 @@ def main() -> None:
     dispatcher = updater.dispatcher
 
     # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
-    conv_handler = ConversationHandler(
+    conv_handler_start = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
         states={
-            PANTRY: [MessageHandler(Filters.regex('^(Block D|Block E)$'), pantry)],
-            PERISHABLE: [MessageHandler(Filters.regex('^(Perishable|Non-perishable)$'), perishable)],
-            PHOTO: [MessageHandler(Filters.photo, photo)],
-            FEEDBACK: [
-                MessageHandler(Filters.text & ~Filters.command, feedback),
-                CommandHandler('skip', skip_feedback),
+            PANTRY_START: [MessageHandler(Filters.regex('^(Block D|Block E)$'), pantry_start)],
+            PERISHABLE_START: [MessageHandler(Filters.regex('^(Perishable|Non-perishable)$'), perishable_start)],
+            PHOTO_START: [MessageHandler(Filters.photo, photo_start)],
+            FEEDBACK_START: [
+                MessageHandler(Filters.text & ~Filters.command, feedback_start),
+                CommandHandler('skip', skip_feedback_start),
             ],
         },
         fallbacks=[CommandHandler('cancel', cancel)],
     )
 
-    dispatcher.add_handler(conv_handler)
-    # dispatcher.add_handler(CommandHandler('help', help))
-    # dispatcher.add_handler(MessageHandler(Filters.command, unknown))
+    conv_handler_update = ConversationHandler(
+        entry_points=[CommandHandler('update', status)],
+        states={
+            PANTRY_UPDATE: [MessageHandler(Filters.regex('^(Block D|Block E)$'), pantry_update)],
+            PHOTO_UPDATE: [MessageHandler(Filters.photo, photo_update)],
+            FEEDBACK_UPDATE: [
+                MessageHandler(Filters.text & ~Filters.command, feedback_update),
+                CommandHandler('skip', skip_feedback_update),
+            ],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+    )
+
+    dispatcher.add_handler(conv_handler_start)
+    dispatcher.add_handler(conv_handler_update)
 
     # Start the Bot
     updater.start_polling()
