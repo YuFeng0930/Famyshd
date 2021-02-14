21ef0958747e9139bf827efe9743268b324cbc04
YuFeng <leo20145kl@gmail.com>
2021-02-14 22:12:44 +0800

    Fix multiple users bug

--- bot.py
+++ bot.py
@@ -316,12 +316,12 @@ def main() -> None:
     dispatcher.add_handler(conv_handler_start)
     dispatcher.add_handler(conv_handler_update)
 
-    updater.start_polling()
-    '''# Start the Bot
+    # updater.start_polling()
+    # Start the Bot
     updater.start_webhook(listen="0.0.0.0",
                         port=int(PORT),
                         url_path=TOKEN)
-    updater.bot.setWebhook('https://blooming-everglades-53145.herokuapp.com/' + TOKEN)'''
+    updater.bot.setWebhook('https://blooming-everglades-53145.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
